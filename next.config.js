16623d866bdd933d50b8de7ef2d9fe168bb524fb
lgao066 <lgao066@gmail.com>
2025-04-26 23:00:54 +1000

    fix: remove invalid server configuration from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  server: {
-    host: '0.0.0.0',
-    port: 3000
-  }
+  reactStrictMode: true
 };
 
 module.exports = nextConfig; 
